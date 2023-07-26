0e28a6b7629914864883a7c0233f477bf20afb36
joonashak <joonashak@proton.me>
2023-07-26 20:35:39 +0300

    Use express-session

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import * as session from "express-session";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: "my-secret",
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
