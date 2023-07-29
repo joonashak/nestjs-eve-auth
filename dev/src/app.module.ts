5cb8c27a21d1dc0551c969faaa8b8c77243afd1b
joonashak <joonashak@proton.me>
2023-07-29 22:52:06 +0300

    Clean up dev

--- dev/src/app.module.ts
+++ dev/src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from "@nestjs/common";
 import { EveAuthModule } from "nestjs-eve-auth";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
-    AuthModule,
     EveAuthModule.forRoot({
       clientId: process.env.CLIENT_ID,
       secretKey: process.env.SECRET_KEY,
