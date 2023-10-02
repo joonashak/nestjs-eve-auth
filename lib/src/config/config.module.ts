d14e57b3320f34889e6d5f0f9da82433fc47a3f4
joonashak <joonashak@proton.me>
2023-10-02 23:12:02 +0300

    Change config module to explicit dependency instead of global module

--- lib/src/config/config.module.ts
+++ lib/src/config/config.module.ts
@@ -1,8 +1,7 @@
-import { Global, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigService } from "./config.service";
 import { Consumer } from "./consumer.service";
 
-@Global()
 @Module({
   providers: [ConfigService, Consumer],
   exports: [ConfigService, Consumer],
--- lib/src/sso/sso.module.ts
+++ lib/src/sso/sso.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
+import { ConfigModule } from "../config/config.module";
 import { OAuthStrategy } from "./oauth.strategy";
 import { SsoService } from "./sso.service";
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, ConfigModule],
   providers: [OAuthStrategy, SsoService],
   exports: [SsoService],
 })
