fe600445b2fec1fffa4680503d61c9a83d916ef0
joonashak <joonashak@proton.me>
2023-10-29 19:56:36 +0200

    Add tests for SSO token verification and revocation

--- lib/src/sso/eve-sso.service.spec.ts
+++ lib/src/sso/eve-sso.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test } from "@nestjs/testing";
 import axios from "axios";
-import { InvalidRefreshTokenException } from "../exceptions";
+import {
+  InvalidAccessTokenException,
+  InvalidRefreshTokenException,
+} from "../exceptions";
 import {
   defaultMockConfiguration,
   provideMockConfigService,
@@ -47,6 +50,24 @@ describe("EveSsoService", () => {
     );
   });
 
+  it("Revokes refresh tokens correctly", async () => {
+    const refreshToken = "n0vw8rf6";
+    mockedAxios.post.mockResolvedValueOnce({
+      data: { access_token: accessToken, refresh_token: refreshToken },
+    });
+
+    const test = async () => eveSsoService.revokeRefreshToken(refreshToken);
+    await expect(test()).resolves.toBeUndefined();
+
+    expect(axios.post).toBeCalledWith(
+      defaultMockConfiguration.revocationUrl,
+      expect.objectContaining({
+        _streams: expect.arrayContaining(["refresh_token", refreshToken]),
+      }),
+      expect.anything(),
+    );
+  });
+
   describe("Refresh tokens", () => {
     it("Refreshes tokens correctly", async () => {
       const oldRefreshToken = "dj0q93h2+0f";
@@ -73,6 +94,31 @@ describe("EveSsoService", () => {
       mockedAxios.post.mockRejectedValueOnce({ response: { status: 400 } });
       const test = async () => eveSsoService.refreshTokens("");
       await expect(test()).rejects.toThrow(InvalidRefreshTokenException);
+    });
+  });
+
+  describe("Verify SSO token", () => {
+    it("Verifies SSO token and returns decoded data", async () => {
+      const accessToken = "jd0283hb2";
+      const data = "asd";
+      mockedAxios.get.mockResolvedValueOnce({ data });
+
+      const test = async () =>
+        eveSsoService.verifyAndDecodeSsoAccessToken(accessToken);
+      await expect(test()).resolves.toEqual(data);
+
+      expect(axios.get).toBeCalledWith(
+        defaultMockConfiguration.verifyUrl,
+        expect.objectContaining({
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }),
+      );
+    });
+
+    it("Throws for failed token verification", async () => {
+      mockedAxios.get.mockRejectedValueOnce({});
+      const test = async () => eveSsoService.verifyAndDecodeSsoAccessToken("");
+      await expect(test()).rejects.toThrow(InvalidAccessTokenException);
     });
   });
 });
